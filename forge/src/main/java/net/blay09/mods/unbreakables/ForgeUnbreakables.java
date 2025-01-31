44b477422c95ea4c29ff7373c904af18e6001a00
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-31 17:03:23 +0100

    fix: Fix Forge and NeoForge compile

--- forge/src/main/java/net/blay09/mods/unbreakables/ForgeUnbreakables.java
+++ forge/src/main/java/net/blay09/mods/unbreakables/ForgeUnbreakables.java
@@ -1,16 +1,14 @@
 package net.blay09.mods.unbreakables;
 
 import net.blay09.mods.balm.api.Balm;
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.blay09.mods.balm.api.EmptyLoadContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod(Unbreakables.MOD_ID)
 public class ForgeUnbreakables {
 
     public ForgeUnbreakables() {
-        Balm.initialize(Unbreakables.MOD_ID, Unbreakables::initialize);
+        Balm.initialize(Unbreakables.MOD_ID, EmptyLoadContext.INSTANCE, Unbreakables::initialize);
     }
 
 }
