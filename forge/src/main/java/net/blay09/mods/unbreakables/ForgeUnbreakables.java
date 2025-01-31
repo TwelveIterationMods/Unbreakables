be25f9df62474092d4c365616d871667bec19890
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-31 17:01:40 +0100

    fix: Fix Forge and NeoForge compile

--- forge/src/main/java/net/blay09/mods/unbreakables/ForgeUnbreakables.java
+++ forge/src/main/java/net/blay09/mods/unbreakables/ForgeUnbreakables.java
@@ -1,16 +1,16 @@
 package net.blay09.mods.unbreakables;
 
 import net.blay09.mods.balm.api.Balm;
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.blay09.mods.balm.forge.ForgeLoadContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Unbreakables.MOD_ID)
 public class ForgeUnbreakables {
 
-    public ForgeUnbreakables() {
-        Balm.initialize(Unbreakables.MOD_ID, Unbreakables::initialize);
+    public ForgeUnbreakables(FMLJavaModLoadingContext context) {
+        final var loadContext = new ForgeLoadContext(context.getModEventBus());
+        Balm.initialize(Unbreakables.MOD_ID, loadContext, Unbreakables::initialize);
     }
 
 }
--- neoforge/src/main/java/net/blay09/mods/unbreakables/NeoForgeUnbreakables.java
+++ neoforge/src/main/java/net/blay09/mods/unbreakables/NeoForgeUnbreakables.java
@@ -1,33 +1,15 @@
-package net.blay09.mods.waystones;
+package net.blay09.mods.unbreakables;
 
 import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.neoforge.NeoForgeLoadContext;
-import net.blay09.mods.waystones.compat.Compat;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.lang.reflect.InvocationTargetException;
+@Mod(Unbreakables.MOD_ID)
+public class NeoForgeUnbreakables {
 
-@Mod(Waystones.MOD_ID)
-public class NeoForgeWaystones {
-
-    private static final Logger logger = LoggerFactory.getLogger(NeoForgeWaystones.class);
-
-    public NeoForgeWaystones(IEventBus modEventBus) {
+    public NeoForgeUnbreakables(IEventBus modEventBus) {
         final var context = new NeoForgeLoadContext(modEventBus);
-        Balm.initialize(Waystones.MOD_ID, context, Waystones::initialize);
-
-        Balm.initializeIfLoaded(Compat.THEONEPROBE, "net.blay09.mods.waystones.compat.TheOneProbeIntegration");
-
-        // TODO would be nice if we could use Balm.initializeIfLoaded here, but it might run too late at the moment)
-        if (Balm.isModLoaded("repurposed_structures")) {
-            try {
-                Class.forName("net.blay09.mods.waystones.compat.RepurposedStructuresIntegration").getConstructor().newInstance();
-            } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | ClassNotFoundException | InvocationTargetException e) {
-                logger.error("Failed to load Repurposed Structures integration", e);
-            }
-        }
+        Balm.initialize(Unbreakables.MOD_ID, context, Unbreakables::initialize);
     }
 }
