c50ba9c315072eb57c6b62d123d8c6436583b420
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-01-31 16:59:50 +0100

    fix: Remove obsolete imports

--- forge/src/main/java/net/blay09/mods/unbreakables/ForgeUnbreakables.java
+++ forge/src/main/java/net/blay09/mods/unbreakables/ForgeUnbreakables.java
@@ -1,10 +1,7 @@
 package net.blay09.mods.unbreakables;
 
 import net.blay09.mods.balm.api.Balm;
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.network.NetworkConstants;
 
 @Mod(Unbreakables.MOD_ID)
 public class ForgeUnbreakables {
