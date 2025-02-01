3368999fbfa2cbf15d64d40eaf516aadc75ef3f6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-01 13:37:56 +0100

    feat: Add level and pos to break context

--- common/src/main/java/net/blay09/mods/unbreakables/api/BreakContext.java
+++ common/src/main/java/net/blay09/mods/unbreakables/api/BreakContext.java
@@ -1,7 +1,9 @@
 package net.blay09.mods.unbreakables.api;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
 
 public interface BreakContext {
@@ -10,6 +12,10 @@ public interface BreakContext {
     float getContextValue(ResourceLocation id);
 
     Player getPlayer();
+
+    BlockGetter getBlockGetter();
+
+    BlockPos getPos();
 
     BlockState getState();
 }
