e95ca9192aab13bfbe24fcf6ce105440d06b072e
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-01 13:52:58 +0100

    feat: Add TaggableIdParameter

---
+++ common/src/main/java/net/blay09/mods/unbreakables/rules/parameters/TaggableIdParameter.java
@@ -0,0 +1,6 @@
+package net.blay09.mods.unbreakables.rules.parameters;
+
+import net.minecraft.resources.ResourceLocation;
+
+public record TaggableIdParameter(ResourceLocation value, boolean isTag) {
+}
