840a55b0dec304fc0708cb76605827146145b744
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-01 17:38:58 +0100

    feat: Add is_at, is_near, is_within, is_below_y, is_above_y conditions

---
+++ common/src/main/java/net/blay09/mods/unbreakables/rules/parameters/BoundsParameter.java
@@ -0,0 +1,4 @@
+package net.blay09.mods.unbreakables.rules.parameters;
+
+public record BoundsParameter(IntParameter minX, IntParameter minY, IntParameter minZ, IntParameter maxX, IntParameter maxY, IntParameter maxZ) {
+}
