e61a6fce4d1cefc067ef750954d6e5335008a7c9
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-19 22:41:27 +0100

    feat: Add BreakContext.viaServer for running server-side conditions #20

--- common/src/main/java/net/blay09/mods/unbreakables/api/BreakContext.java
+++ common/src/main/java/net/blay09/mods/unbreakables/api/BreakContext.java
@@ -2,9 +2,14 @@ package net.blay09.mods.unbreakables.api;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public interface BreakContext {
     <P> boolean matchesCondition(ConfiguredCondition<P> configuredCondition);
@@ -18,4 +23,6 @@ public interface BreakContext {
     BlockPos getPos();
 
     BlockState getState();
+
+    boolean viaServer(Function<ServerLevel, Boolean> runner);
 }
