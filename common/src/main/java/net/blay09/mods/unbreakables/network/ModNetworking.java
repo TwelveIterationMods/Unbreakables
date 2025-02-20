746332169ad58328d6e21746b2788b1d2aa349ac
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-02-20 20:48:03 +0100

    feat: Allow server-only

--- common/src/main/java/net/blay09/mods/unbreakables/network/ModNetworking.java
+++ common/src/main/java/net/blay09/mods/unbreakables/network/ModNetworking.java
@@ -1,10 +1,13 @@
 package net.blay09.mods.unbreakables.network;
 
 import net.blay09.mods.balm.api.network.BalmNetworking;
+import net.blay09.mods.unbreakables.Unbreakables;
 
 public class ModNetworking {
 
     public static void initialize(BalmNetworking networking) {
+        networking.allowServerOnly(Unbreakables.MOD_ID);
+
         networking.registerClientboundPacket(UnbreakableRulesMessage.TYPE, UnbreakableRulesMessage.class, UnbreakableRulesMessage::encode, UnbreakableRulesMessage::decode, UnbreakableRulesMessage::handle);
         networking.registerClientboundPacket(ClientboundUnbreakableStatusPacket.TYPE, ClientboundUnbreakableStatusPacket.class, ClientboundUnbreakableStatusPacket::encode, ClientboundUnbreakableStatusPacket::decode, ClientboundUnbreakableStatusPacket::handle);
     }
