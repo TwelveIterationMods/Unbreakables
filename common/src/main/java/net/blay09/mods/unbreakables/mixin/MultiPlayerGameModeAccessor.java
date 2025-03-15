182f07afbdf61d773d6e29fbc3760a1b493c25f4
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-15 17:54:34 +0100

    fix: Fix break hints showing even while not breaking

---
+++ common/src/main/java/net/blay09/mods/unbreakables/mixin/MultiPlayerGameModeAccessor.java
@@ -0,0 +1,13 @@
+package net.blay09.mods.unbreakables.mixin;
+
+import net.minecraft.client.multiplayer.MultiPlayerGameMode;
+import net.minecraft.core.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(MultiPlayerGameMode.class)
+public interface MultiPlayerGameModeAccessor {
+
+    @Accessor
+    BlockPos getDestroyBlockPos();
+}
