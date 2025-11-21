213113906fd14bbf4a58c53ee385f4a3ead16342
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-21 14:45:17 +0100

    feat: Update to Minecraft 1.21.11

--- neoforge/src/main/java/net/blay09/mods/unbreakables/NeoForgeUnbreakables.java
+++ neoforge/src/main/java/net/blay09/mods/unbreakables/NeoForgeUnbreakables.java
@@ -1,7 +1,7 @@
 package net.blay09.mods.unbreakables;
 
-import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.neoforge.NeoForgeLoadContext;
+import net.blay09.mods.balm.Balm;
+import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
