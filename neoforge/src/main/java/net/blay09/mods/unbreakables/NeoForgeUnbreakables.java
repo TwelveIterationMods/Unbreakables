b80efd203944e896a76bfc9de5ad0dc4b7ed147c
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-22 18:56:29 +0100

    feat: Update to Minecraft 26.1

--- neoforge/src/main/java/net/blay09/mods/unbreakables/NeoForgeUnbreakables.java
+++ neoforge/src/main/java/net/blay09/mods/unbreakables/NeoForgeUnbreakables.java
@@ -3,13 +3,14 @@ package net.blay09.mods.unbreakables;
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(Unbreakables.MOD_ID)
 public class NeoForgeUnbreakables {
 
-    public NeoForgeUnbreakables(IEventBus modEventBus) {
-        final var context = new NeoForgeLoadContext(modEventBus);
+    public NeoForgeUnbreakables(ModContainer modContainer, IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, modEventBus);
         Balm.initializeMod(Unbreakables.MOD_ID, context, Unbreakables::initialize);
     }
 }
--- neoforge/src/main/java/net/blay09/mods/unbreakables/client/NeoForgeUnbreakablesClient.java
+++ neoforge/src/main/java/net/blay09/mods/unbreakables/client/NeoForgeUnbreakablesClient.java
@@ -5,13 +5,14 @@ import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.blay09.mods.unbreakables.Unbreakables;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(value = Unbreakables.MOD_ID, dist = Dist.CLIENT)
 public class NeoForgeUnbreakablesClient {
 
-    public NeoForgeUnbreakablesClient(IEventBus modEventBus) {
-        final var context = new NeoForgeLoadContext(modEventBus);
+    public NeoForgeUnbreakablesClient(ModContainer modContainer, IEventBus modEventBus) {
+        final var context = new NeoForgeLoadContext(modContainer, modEventBus);
         BalmClient.initializeMod(Unbreakables.MOD_ID, context, UnbreakablesClient::initialize);
     }
 }
