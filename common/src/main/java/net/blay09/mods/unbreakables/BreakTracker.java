a8e459303ad3ab832fbc2fd9057b816fcf339cc4
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-27 09:55:56 +0100

    refactor: Remove jetbrains annotations

--- common/src/main/java/net/blay09/mods/unbreakables/BreakTracker.java
+++ common/src/main/java/net/blay09/mods/unbreakables/BreakTracker.java
@@ -4,7 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 import java.util.*;
 import java.util.function.Consumer;
